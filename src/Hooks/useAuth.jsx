3e4f8872063e5a7418a1e92ab772045c5a2735ad
walid-official <walid.official8@gmail.com>
2025-01-15 19:27:26 +0600

    custom hook is added

---
+++ src/Hooks/useAuth.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react"
+import { AuthContext } from './../components/AuthProvider/AuthProvider';
+
+
+const useAuth = () => {
+    const auth = useContext(AuthContext)
+    return auth
+}
+
+export default useAuth;
