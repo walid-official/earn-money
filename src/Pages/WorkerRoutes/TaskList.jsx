74b349a3c678b6b62fb38459aa0bf53304735681
walid-official <walid.official8@gmail.com>
2025-01-17 12:41:09 +0600

    taskList data fetching is done

--- src/Pages/WorkerRoutes/TaskList.jsx
+++ src/Pages/WorkerRoutes/TaskList.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const TaskList = ({postedTask}) => {
-    const {} = postedTask || {}
-    return (
-        <div>
-            
+const TaskList = ({ postedTask }) => {
+  const {
+    title,
+    detail,
+    payment,
+    worker,
+    totalPayment,
+    completionDate,
+    submissionImage,
+    taskImage,
+    buyerInfo,
+  } = postedTask || {};
+
+  let date = new Date(completionDate);
+  /* Date format you have */
+  let dateMDY = `${date.getDate()}-${
+    date.getMonth() + 1
+  }-${date.getFullYear()}`;
+  console.log(dateMDY);
+
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-xl">
+        <figure>
+          <img src={taskImage} alt="Shoes" />
+        </figure>
+        <div className="px-4 py-4 space-y-3">
+          <div className="flex items-center justify-between">
+            <h2 className="font-bold text-xl">{title}</h2>
+            <p className="bg-accent text-white px-5 py-2 rounded-lg">{worker}</p>
+          </div>
+          <p className="font-bold">{buyerInfo?.name}</p>
+          <p className="border-b pb-3">{dateMDY}</p>
+
+          <div className="card-actions justify-between items-center ">
+            <p className="font-bold text-xl">{payment}.00$</p>
+            <button className="btn bg-accent text-white">view Details</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
--- src/Pages/WorkerRoutes/TaskLists.jsx
+++ src/Pages/WorkerRoutes/TaskLists.jsx
@@ -1,29 +1,34 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import useAxiosSecure from '../../Hooks/useAxiosSecure';
-import TaskList from './TaskList';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import TaskList from "./TaskList";
 
 const TaskLists = () => {
-const axiosSecure = useAxiosSecure()
-    const { data: postedTasks = [], isLoading,refetch } = useQuery({
-        queryKey: ["myTasks"],
-        queryFn: async () => {
-          const { data } = await axiosSecure.get(`postedTasks`);
-          console.log(data);
-          return data;
-        },
-      });
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: postedTasks = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["myTasks"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`postedTasks`);
+      console.log(data);
+      return data;
+    },
+  });
 
-console.log(postedTasks);
+  console.log(postedTasks);
 
-
-    return (
-        <div>
-            {
-                postedTasks.map((postedTask,index) => <TaskList key={index} postedTask={postedTask}></TaskList>)
-            }
-        </div>
-    );
+  return (
+    <div className="w-11/12 mx-auto py-20">
+      <div className="grid grid-cols-3 gap-4">
+        {postedTasks.map((postedTask, index) => (
+          <TaskList key={index} postedTask={postedTask}></TaskList>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TaskLists;
