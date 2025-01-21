5d10b42c35b2c434db9fef76c023249af0c8444c
walid-official <walid.official8@gmail.com>
2025-01-21 23:39:23 +0600

    photo url in imgbb is done

--- src/Pages/Authentications/Register.jsx
+++ src/Pages/Authentications/Register.jsx
@@ -2,57 +2,55 @@ import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../../components/AuthProvider/AuthProvider";
 import toast from "react-hot-toast";
-import { Navigate, NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 import axios from "axios";
 
+const Image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${Image_hosting_key}`;
+
 const Register = () => {
-  const { createUser, updateUserProfile, signInWithGoogle } =
-    useContext(AuthContext);
-    const navigate = useNavigate()
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+  const { createUser, updateUserProfile, signInWithGoogle } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
   const onSubmit = async (data) => {
     console.log(data);
-    const { name, photo, email, password } = data;
+    const { name, email, password, role } = data;
 
     let coin;
-    if (data.role === "Worker") {
+    if (role === "Worker") {
       coin = 10;
     }
-    if (data.role === "Buyer") {
+    if (role === "Buyer") {
       coin = 50;
     }
 
-    const userData = {
-      name: data.name,
-      photo: data.photo,
-      email: data.email,
-      role: data.role,
-      coin: coin,
-    };
+    const formData = new FormData();
+    formData.append('image', data.photo[0]);
 
     try {
+      // Upload image to imgbb
+      const imgResponse = await axios.post(image_hosting_api, formData);
+      const photoURL = imgResponse.data.data.display_url;
+
+      // Proceed with user creation
       const userCredential = await createUser(email, password);
-      await updateUserProfile({
-        displayName: name,
-        photoURL: photo,
-      });
+      await updateUserProfile({ displayName: name, photoURL });
       console.log("User Registered", userCredential);
 
-      const { data } = await axios.post(
-        "http://localhost:5000/earning-users",
-        userData
-      );
-      console.log(data);
+      const userData = {
+        name: name,
+        photo: photoURL,
+        email: email,
+        role: role,
+        coin: coin,
+      };
+
+      await axios.post("http://localhost:5000/earning-users", userData);
       toast.success("Successfully Registered Your Account");
       reset();
-      navigate("/")
+      navigate("/");
     } catch (error) {
       console.error("Error Registering User", error);
       toast.error("Registration failed");
@@ -63,7 +61,6 @@ const Register = () => {
     try {
       const data = await signInWithGoogle();
       console.log(data);
-      // Navigate(location?.state || "/");
       const googleUserData = {
         name: data?.user?.displayName,
         email: data?.user?.email,
@@ -82,22 +79,15 @@ const Register = () => {
       <div className="hero bg-gradient-to-t from-[#27292f] to-[#10121d] shadow-2xl rounded-tl-full rounded-br-full w-full min-h-screen">
         <div className="flex gap-8">
           <div className="w-[50%] flex justify-center items-center">
-            <div className="">
-              <h2 className="font-bold text-4xl text-white">
-                Register to Start Earning
-              </h2>
-              <p className="text-white py-3">
-                Sign up now to unlock earning opportunities and access exclusive
-                features.
-              </p>
+            <div>
+              <h2 className="font-bold text-4xl text-white">Register to Start Earning</h2>
+              <p className="text-white py-3">Sign up now to unlock earning opportunities and access exclusive features.</p>
               <div className="flex justify-start py-3 gap-3 items-center">
                 <NavLink to="/login">
-                  <button className="bg-[#00d7c0] rounded-tr-2xl rounded-bl-2xl font-bold text-white hover:bg-[#00d7c0] py-3 px-7">
-                    Go To Login
-                  </button>
+                  <button className="bg-[#00d7c0] rounded-tr-2xl rounded-bl-2xl font-bold text-white hover:bg-[#00d7c0] py-3 px-7">Go To Login</button>
                 </NavLink>
                 <NavLink to="/">
-                  <button className="text-white font-bold flex items-center gap-2 hover:translate-x-2 duration-500 ">
+                  <button className="text-white font-bold flex items-center gap-2 hover:translate-x-2 duration-500">
                     Go To Home
                     <FaArrowRightLong />
                   </button>
@@ -117,23 +107,18 @@ const Register = () => {
                   placeholder="Name"
                   className="input input-bordered"
                 />
-                {errors.name && (
-                  <p className="text-red-500">{errors.name.message}</p>
-                )}
+                {errors.name && <p className="text-red-500">{errors.name.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Photo URL</span>
+                  <span className="label-text">Photo</span>
                 </label>
                 <input
-                  type="text"
-                  {...register("photo", { required: "Photo URL is required" })}
-                  placeholder="Photo URL"
-                  className="input input-bordered"
+                  type="file"
+                  {...register("photo", { required: "Photo is required" })}
+                  className="file-input file-input-bordered w-full max-w-xl"
                 />
-                {errors.photo && (
-                  <p className="text-red-500">{errors.photo.message}</p>
-                )}
+                {errors.photo && <p className="text-red-500">{errors.photo.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -145,9 +130,7 @@ const Register = () => {
                   placeholder="Email"
                   className="input input-bordered"
                 />
-                {errors.email && (
-                  <p className="text-red-500">{errors.email.message}</p>
-                )}
+                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -157,15 +140,11 @@ const Register = () => {
                   {...register("role", { required: "Role is required" })}
                   className="select select-bordered w-full max-w-sm"
                 >
-                  <option disabled selected value="">
-                    Choose Your Role
-                  </option>
+                  <option disabled selected value="">Choose Your Role</option>
                   <option value="Worker">Worker</option>
                   <option value="Buyer">Buyer</option>
                 </select>
-                {errors.role && (
-                  <p className="text-red-500">{errors.role.message}</p>
-                )}
+                {errors.role && <p className="text-red-500">{errors.role.message}</p>}
               </div>
               <div className="form-control">
                 <label className="label">
@@ -177,23 +156,18 @@ const Register = () => {
                     required: "Password is required",
                     pattern: {
                       value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/,
-                      message:
-                        "Password must be at least 6 characters, include one uppercase letter, one lowercase letter, and one number",
+                      message: "Password must be at least 6 characters, include one uppercase letter, one lowercase letter, and one number",
                     },
                   })}
                   placeholder="Password"
                   className="input input-bordered"
                 />
-                {errors.password && (
-                  <p className="text-red-500">{errors.password.message}</p>
-                )}
+                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
               </div>
               <div className="form-control mt-6">
-                <button type="submit" className="btn bg-[#00d7c0] hover:bg-[#00d7c0] border-none text-white font-medium">
-                  Register Now
-                </button>
+                <button type="submit" className="btn bg-[#00d7c0] hover:bg-[#00d7c0] border-none text-white font-medium">Register Now</button>
               </div>
-              <div className="">
+              <div>
                 <button
                   onClick={handleGoogleRegister}
                   type="button"
