667dc6023f814dd236bb107dd15740160a247acd
walid-official <walid.official8@gmail.com>
2025-07-08 12:03:30 +0600

    base url changes are done

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import useAuth from './useAuth';
 
 
 export const axiosSecure = axios.create({
-    baseURL: "https://earn-money-server-5.onrender.com/",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000"
 })
 
 const useAxiosSecure = () => {
